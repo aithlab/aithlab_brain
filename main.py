ac60f817784b582408a4eb30f3ccc4e547b3fe5f
aithlab <kteaw0110@gmail.com>
2024-05-13 21:30:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from datetime import datetime
-
-from utils_gs import get_df_from_url, authorize_with_credentials, open_gs_by_url, save_df_to_gd, get_session_id
+from langchain_community.utilities import pebblo
+from utils_gs import get_df_from_url, authorize_with_credentials, open_gs_by_url, save_df_to_gd, get_session_id, get_document
 from utils_llm import get_retriever, get_llm_model, get_chat_chain
+
+ip = pebblo.get_ip() 
 
 # Streamlit
 st.set_page_config(
@@ -42,7 +44,10 @@ if 'session_id' not in st.session_state:
     st.session_state.session_id = get_session_id(_df_qna)
 
 llm_model = get_llm_model()
-path = "./CV_TaehwanKim_v2.txt"
+# path = "./CV_TaehwanKim_v2.txt"
+# path = st.secrets['rag_url']
+document_id = st.secrets['document_id']
+path = get_document(credentials, document_id)
 retriever = get_retriever(path)
 st.session_state.conversation = get_chat_chain(st.session_state.store, st.session_state.session_id,
                                           retriever, llm_model)
@@ -63,24 +68,30 @@ for message in st.session_state.messages:
 query = st.chat_input("Please enter here")
 if query:
     _session_id = st.session_state.session_id
+    st.session_state.messages.append({"role": "user", "content": query})
     with st.chat_message("user"):
         st.markdown(query)
     
     with st.chat_message("assistant"):
+        chat_container = st.empty()
         _chain = st.session_state.conversation
         with st.spinner("Thinking..."):
-            result = _chain.invoke(
+            answer = _chain.stream(
                 {'question':query},
                 config={'configurable':{"session_id":_session_id}}
             )
-            response = result.content
-            st.markdown(response)
+            # response = result.content
+            # st.markdown(response)
+            chunks = []
+            for chunk in answer:
+                chunks.append(chunk)
+                chat_container.markdown("".join(chunks))
     
-    st.session_state.messages.append({"role": "user", "content": query})
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    # st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": "".join(chunks)})
     
     _df_qna = st.session_state.df_qna
     date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    _df_qna.loc[len(_df_qna)] = [date, _session_id, query, response]
+    _df_qna.loc[len(_df_qna)] = [ip, date, _session_id, query, "".join(chunks)]
     save_df_to_gd(_df_qna, worksheet)
-    print(st.session_state.store)
+    # print(st.session_state.store)
